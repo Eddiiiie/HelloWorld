a00deb79dcd22addc3920d9e1a86f1eb3f765edc
Shortytheman <shor7ty@gmail.com>
2021-09-29 10:32:42 +0200

    Ggwp

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
+
 public class helloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hvad sker der");
 
         System.out.println("Vaadrut was here");
+        Scanner scanner = new Scanner(System.in);
+        int g = scanner.nextInt();
+        System.out.println(g);
     }
 }
